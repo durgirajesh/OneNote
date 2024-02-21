bcf1e0e0050a43bce09589840044bce6cf9cc0d4
durgirajesh <durgirajesh@gmail.com>
2024-02-21 16:13:42 +0530

    first commit

--- OneNote/tasks/urls.py
+++ OneNote/tasks/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.list_view, name='list_view'), # type: ignore
-    path('update', views.update_view, name='update_view')
-=======
     path('', views.list_view, name='list_view'),
     path('update', views.update_view, name='update_view'),
     path('delete', views.delete_view, name='delete_view')
->>>>>>> 545d61fcc295fc883899ad807bd299f036e1808e
 ]
